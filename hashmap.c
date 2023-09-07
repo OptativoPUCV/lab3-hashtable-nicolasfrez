552b4c8c83d3525446927d108cb850ce7e66db16
Replit user <>
2023-09-07 17:21:30 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,7 +41,7 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value) 
 {
-  int indice = hash(key, key->capacity);
+  int indice = hash(key, map->capacity);
   while(map->buckets[indice] != NULL || map->buckets[indice]->key!=NULL)
     {
       if(is_equal(map->buckets[indice]->key, key))
