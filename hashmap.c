71991770b330bc37786ec55a86835fd46d2e6bc1
Replit user <>
2023-09-07 17:25:04 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -44,7 +44,7 @@ void insertMap(HashMap * map, char * key, void * value)
   
   long
   indice = hash(key, map->capacity);
-  while(map->buckets[indice] != NULL || map->buckets[indice]->key!=NULL)
+  while(map->buckets[indice] != NULL && map->buckets[indice]->key!=NULL)
     {
       if(is_equal(map->buckets[indice]->key, key))
       {
